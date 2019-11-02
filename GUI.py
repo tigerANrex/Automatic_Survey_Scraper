4b57034da19f51e5e032e89bbe52731be33b5154
andrewon <dnta@memphis.edu>
2019-11-02 14:05:57 -0500

    change logo, added menu bar

--- GUI.py
+++ GUI.py
@@ -7,13 +7,13 @@ root = Tk()
 root.geometry('220x230+500+100') 
 
 #dropdown menu
-Options=["McDonnald's", 'Burger King']
+Options=["McDonald's", 'Burger King']
 
 restaurant = StringVar(root)
 restaurant.set(Options[0])#default option
 
 #logo
-photo = PhotoImage(file = r'C:\Users\ducta\Documents\VandyHacks\Automatic_Survey_scraper\logo_cropped.png')
+photo = PhotoImage(file = r'C:\Users\ducta\Documents\VandyHacks\Automatic_Survey_scraper\logo_cropped1.png')
 
 logolabel = Label(root,image=photo).pack(side= TOP, pady = 0)
 
@@ -38,7 +38,16 @@ pathlabel.pack(side = TOP, pady = 5)
 button2 = Button(root, text = 'Receipt', command= open_image).pack(side = TOP, pady = 5) 
 button = Button(root, text = 'Start', command= click_here_info).pack(side = TOP, pady = (5,10)) 
 
-
+#menu bar
+menubar = Menu (root)
+#file
+filemenu = Menu(menubar, tearoff = 0)
+filemenu.add_command(label="Open File", command=NONE)
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=root.quit)
+menubar.add_cascade(label="File", menu = filemenu)
+#display the menu
+root.config(menu=menubar)
 
 
 mainloop() 
