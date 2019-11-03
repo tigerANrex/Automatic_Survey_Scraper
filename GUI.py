5977e9c2e391ad115eb7fd375e4f881e38cc86a5
andrewon <dnta@memphis.edu>
2019-11-02 20:26:43 -0500

    reorganized

--- GUI.py
+++ GUI.py
@@ -1,11 +1,14 @@
 from tkinter import *
 from tkinter import messagebox
 from tkinter import filedialog
+import os
 
 # creating tkinter window 
 root = Tk() 
-root.geometry('220x230+500+100') 
-
+root.geometry('220x220+500+100') 
+#overall frame
+overall_frame=Frame(root,borderwidth=2,relief='ridge')
+overall_frame.pack(side=TOP)
 #dropdown menu
 Options=["McDonald's", 'Burger King']
 
@@ -14,29 +17,39 @@ restaurant.set(Options[0])#default option
 
 #logo
 photo = PhotoImage(file = r'C:\Users\ducta\Documents\VandyHacks\Automatic_Survey_scraper\logo_cropped1.png')
+logolabel = Label(overall_frame,image=photo).pack(side= TOP, pady = (10,2))
 
-logolabel = Label(root,image=photo).pack(side= TOP, pady = 0)
 
-r=OptionMenu(root,restaurant,*Options)
-r.pack(side = TOP, pady = (1,2))
+
+#frame for pathlabel and browse button
+centerframe = Frame(overall_frame)
+centerframe.pack(side = TOP, padx = 5)
 
 def click_here_info():
     messagebox.showinfo('hey','xin chao!')
 
 
 def open_image():
-    filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
+    filepath =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
+    filename = os.path.basename(filepath)
     pathlabel.config(text=filename)
 
 
+button2 = Button(centerframe, text = 'Browse', command= open_image, height=1).pack(side = RIGHT, padx=3) 
+pathlabel= Label(centerframe, text="Click 'Browse' to find file",width = 20, pady = 3, fg='gray')
+pathlabel.config( borderwidth=2,relief='ridge', bg="white", font=("",8,'italic'))
+pathlabel.pack(side = RIGHT,)
 
-pathlabel= Label(root, width = 20)
-pathlabel.pack(side = TOP, pady = 5)
 
+#frame
+buttomframe = Frame(overall_frame, pady=30)
+buttomframe.pack(side=TOP)
 
+button = Button(buttomframe, text = 'Start', command= click_here_info).pack(side = RIGHT) 
+#dropdown
+r=OptionMenu(buttomframe,restaurant,*Options)
+r.pack(side = RIGHT)
 
-button2 = Button(root, text = 'Receipt', command= open_image).pack(side = TOP, pady = 5) 
-button = Button(root, text = 'Start', command= click_here_info).pack(side = TOP, pady = (5,10)) 
 
 #menu bar
 menubar = Menu (root)
